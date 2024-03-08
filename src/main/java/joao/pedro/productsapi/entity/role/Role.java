11c450437bf139ff2beae31654a50661266081d8
joaosantosmedeiros <jopesame@gmail.com>
2024-03-07 21:11:53 -0300

    feat: add role class

---
+++ src/main/java/joao/pedro/productsapi/entity/role/Role.java
@@ -0,0 +1,45 @@
+package joao.pedro.productsapi.entity.role;
+
+import java.util.Objects;
+import java.util.UUID;
+
+public class Role {
+
+    private final UUID id;
+    private String name;
+
+    public Role(String name) {
+        this.id = UUID.randomUUID();
+        this.name = name;
+    }
+
+    public Role(UUID id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Role role = (Role) o;
+        return Objects.equals(id, role.id) && Objects.equals(name, role.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+}
