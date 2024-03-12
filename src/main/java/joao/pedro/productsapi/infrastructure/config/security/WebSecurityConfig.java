be313c8dae14039058d4baf720a50c67d00077ad
joaosantosmedeiros <jopesame@gmail.com>
2024-03-12 09:33:56 -0300

    feat: add spring security dependency

---
+++ src/main/java/joao/pedro/productsapi/infrastructure/config/security/WebSecurityConfig.java
@@ -0,0 +1,25 @@
+package joao.pedro.productsapi.infrastructure.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.csrf(csrf -> csrf.disable()).
+                authorizeHttpRequests(request -> request
+                        .requestMatchers(HttpMethod.POST, "/account").permitAll()
+                        .anyRequest()
+                        .authenticated()
+                );
+
+        return http.build();
+    }
+}
