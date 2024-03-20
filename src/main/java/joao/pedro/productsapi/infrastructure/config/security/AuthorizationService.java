7759a2c309b948a04342f985e0ca82be84e86f9e
joaosantosmedeiros <jopesame@gmail.com>
2024-03-20 16:22:54 -0300

    feat: add token and authorization service

---
+++ src/main/java/joao/pedro/productsapi/infrastructure/config/security/AuthorizationService.java
@@ -0,0 +1,20 @@
+package joao.pedro.productsapi.infrastructure.config.security;
+
+import joao.pedro.productsapi.infrastructure.config.db.repository.AccountRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class AuthorizationService implements UserDetailsService {
+
+    private AccountRepository accountRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return accountRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("Account not found."));
+    }
+}
---
+++ src/main/java/joao/pedro/productsapi/infrastructure/config/security/TokenService.java
@@ -0,0 +1,49 @@
+package joao.pedro.productsapi.infrastructure.config.security;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import joao.pedro.productsapi.entity.account.model.Account;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
+public class TokenService {
+    @Value("${api.security.token.secret}")
+    private String SECRET;
+
+    public String generateToken(Account account) {
+        try{
+            Algorithm algorithm = Algorithm.HMAC256(SECRET);
+            return JWT.create()
+                    .withIssuer("products-api")
+                    .withSubject(account.getUsername())
+                    .withExpiresAt(generateExpirationDate())
+                    .sign(algorithm);
+        }catch (JWTCreationException exception) {
+            throw new RuntimeException("Error while generating token", exception);
+        }
+    }
+
+    public String validateToken(String token) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(SECRET);
+            return JWT.require(algorithm)
+                    .withIssuer("products-api")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+        }catch (JWTVerificationException exception) {
+            return "";
+        }
+    }
+
+    public Instant generateExpirationDate() {
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+    }
+}
