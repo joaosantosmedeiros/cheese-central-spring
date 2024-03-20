d1114893aa703fa952ba2bb1f01da965a27e3a73
joaosantosmedeiros <jopesame@gmail.com>
2024-03-20 16:26:26 -0300

    feat: implement jwt token authorization

---
+++ src/main/java/joao/pedro/productsapi/infrastructure/config/security/SecurityFilter.java
@@ -0,0 +1,41 @@
+package joao.pedro.productsapi.infrastructure.config.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import joao.pedro.productsapi.infrastructure.config.db.repository.AccountRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@AllArgsConstructor
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+
+    private TokenService tokenService;
+    private AccountRepository accountRepository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        var token = this.recoverToken(request);
+        if(token != null) {
+            var login = tokenService.validateToken(token);
+            UserDetails account = accountRepository.findByUsername(login).orElse(null);
+
+            var authentication = new UsernamePasswordAuthenticationToken(account, null, account != null ? account.getAuthorities() : null);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private String recoverToken(HttpServletRequest request) {
+        var authHeader = request.getHeader("Authorization");
+        return authHeader == null ? null : authHeader.replace("Bearer ", "");
+    }
+}
--- src/main/java/joao/pedro/productsapi/infrastructure/config/security/WebSecurityConfig.java
+++ src/main/java/joao/pedro/productsapi/infrastructure/config/security/WebSecurityConfig.java
@@ -1,25 +1,57 @@
 package joao.pedro.productsapi.infrastructure.config.security;
 
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
+@AllArgsConstructor
 public class WebSecurityConfig {
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-        http.csrf(csrf -> csrf.disable()).
-                authorizeHttpRequests(request -> request
-                        .requestMatchers(HttpMethod.POST, "/account").permitAll()
-                        .anyRequest()
-                        .authenticated()
-                );
+    private SecurityFilter securityFilter;
 
-        return http.build();
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(request -> request
+                        .requestMatchers(HttpMethod.POST, "/auth/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/product/**").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/product/**").hasAnyRole("ADMIN", "ROOT")
+                        .requestMatchers(HttpMethod.PUT, "/product/**").hasAnyRole("ADMIN", "ROOT")
+                        .requestMatchers(HttpMethod.DELETE, "/product/**").hasAnyRole("ADMIN", "ROOT")
+                        .requestMatchers(HttpMethod.GET, "/category/**").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/category/**").hasAnyRole("ADMIN", "ROOT")
+                        .requestMatchers(HttpMethod.PUT, "/category/**").hasAnyRole("ADMIN", "ROOT")
+                        .requestMatchers(HttpMethod.DELETE, "/category/**").hasAnyRole("ADMIN", "ROOT")
+                        .anyRequest()
+                        .authenticated())
+                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
