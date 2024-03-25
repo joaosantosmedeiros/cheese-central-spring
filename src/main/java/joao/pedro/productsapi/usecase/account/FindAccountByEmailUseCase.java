efca408907a6fa9d49f6969ae8f5ddc30fbe929c
joaosantosmedeiros <jopesame@gmail.com>
2024-03-25 19:24:19 -0300

    feat: add find account by email use case

---
+++ src/main/java/joao/pedro/productsapi/usecase/account/FindAccountByEmailUseCase.java
@@ -0,0 +1,25 @@
+package joao.pedro.productsapi.usecase.account;
+
+import joao.pedro.productsapi.entity.account.gateway.AccountGateway;
+import joao.pedro.productsapi.entity.account.model.Account;
+import joao.pedro.productsapi.entity.exceptions.EntityNotFoundException;
+
+import java.util.Optional;
+
+public class FindAccountByEmailUseCase {
+
+    private final AccountGateway accountGateway;
+
+    public FindAccountByEmailUseCase(AccountGateway accountGateway) {
+        this.accountGateway = accountGateway;
+    }
+
+    public Output execute(Input input) {
+        Account account = accountGateway.findByEmail(input.email).orElseThrow(() -> new EntityNotFoundException("Account"));
+
+        return new Output(account);
+    }
+
+    public record Input(String email) {}
+    public record Output(Account data) {}
+}
