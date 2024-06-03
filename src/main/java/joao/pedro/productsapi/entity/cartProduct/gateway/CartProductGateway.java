adccfda7340ba7d998ecc6a62d422b68c1d07713
joaosantosmedeiros <jopesame@gmail.com>
2024-06-03 11:43:08 -0300

    feat: add find by product and cart method in cart prodcut gateway

--- src/main/java/joao/pedro/productsapi/entity/cartProduct/gateway/CartProductGateway.java
+++ src/main/java/joao/pedro/productsapi/entity/cartProduct/gateway/CartProductGateway.java
@@ -1,6 +1,8 @@
 package joao.pedro.productsapi.entity.cartProduct.gateway;
 
+import joao.pedro.productsapi.entity.cart.model.Cart;
 import joao.pedro.productsapi.entity.cartProduct.model.CartProduct;
+import joao.pedro.productsapi.entity.product.model.Product;
 
 import java.util.List;
 import java.util.Optional;
@@ -8,6 +10,8 @@ import java.util.UUID;
 
 public interface CartProductGateway {
     CartProduct create(CartProduct cartProduct);
+
+    Optional<CartProduct> findByProductAndCart(Product product, Cart cart);
 
     Optional<CartProduct> findById(UUID id);
 
