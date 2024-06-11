57d5e41f43221c233c3a85227ffd56cce6af6c91
joaosantosmedeiros <jopesame@gmail.com>
2024-06-11 15:18:23 -0300

    feat: add order database gateway

--- src/main/java/joao/pedro/productsapi/infrastructure/config/db/repository/OrderRepository.java
+++ src/main/java/joao/pedro/productsapi/infrastructure/config/db/repository/OrderRepository.java
@@ -1,9 +1,13 @@
 package joao.pedro.productsapi.infrastructure.config.db.repository;
 
+import joao.pedro.productsapi.infrastructure.config.db.schema.AccountEntity;
 import joao.pedro.productsapi.infrastructure.config.db.schema.OrderEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface OrderRepository extends JpaRepository<OrderEntity, UUID> {
+
+    List<OrderEntity> findByAccount(AccountEntity accountEntity);
 }
---
+++ src/main/java/joao/pedro/productsapi/infrastructure/gateways/order/OrderDatabaseGateway.java
@@ -0,0 +1,38 @@
+package joao.pedro.productsapi.infrastructure.gateways.order;
+
+import joao.pedro.productsapi.entity.account.model.Account;
+import joao.pedro.productsapi.entity.order.gateway.OrderGateway;
+import joao.pedro.productsapi.entity.order.model.Order;
+import joao.pedro.productsapi.infrastructure.config.db.repository.OrderRepository;
+import joao.pedro.productsapi.infrastructure.config.db.schema.OrderEntity;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@Component
+@AllArgsConstructor
+public class OrderDatabaseGateway implements OrderGateway {
+
+    private final OrderRepository orderRepository;
+
+    @Override
+    public Order create(Order order) {
+        orderRepository.save(order.toOrderEntity());
+        return order;
+    }
+
+    @Override
+    public Optional<Order> findById(UUID id) {
+        return orderRepository.findById(id).map(OrderEntity::toOrder);
+    }
+
+    @Override
+    public List<Order> findByAccount(Account account) {
+        return orderRepository.findByAccount(account.toAccountEntity())
+                .stream().map(OrderEntity::toOrder).collect(Collectors.toList());
+    }
+}
