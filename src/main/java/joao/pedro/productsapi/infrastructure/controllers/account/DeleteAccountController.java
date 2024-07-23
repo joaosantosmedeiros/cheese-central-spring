9b24a53e6dd81f0eec0f4317b91439003731bbfe
joaosantosmedeiros <jopesame@gmail.com>
2024-07-23 09:35:55 -0300

    docs: add delete account controller spring doc definition

--- src/main/java/joao/pedro/productsapi/infrastructure/controllers/account/DeleteAccountController.java
+++ src/main/java/joao/pedro/productsapi/infrastructure/controllers/account/DeleteAccountController.java
@@ -1,24 +1,36 @@
 package joao.pedro.productsapi.infrastructure.controllers.account;
 
-import joao.pedro.productsapi.entity.exceptions.NotAuthorizedException;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import joao.pedro.productsapi.infrastructure.config.db.schema.AccountEntity;
 import joao.pedro.productsapi.usecase.account.DeleteAccountUseCase;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @AllArgsConstructor
+@SecurityRequirement(name = "bearer-key")
+@Tag(name = "Accounts", description = "Operations related to accounts.")
 public class DeleteAccountController {
 
     private DeleteAccountUseCase deleteAccountUseCase;
 
     @DeleteMapping("/account")
     @ResponseStatus(HttpStatus.NO_CONTENT)
+    @Operation(description = "Account deletion", summary = "Delete an account.")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "204",
+                    description = "Success."
+            ),
+    })
     public void deleteAccount() {
         var tokenEmail = ((AccountEntity) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getEmail();
         deleteAccountUseCase.execute(new DeleteAccountUseCase.Input(tokenEmail));
