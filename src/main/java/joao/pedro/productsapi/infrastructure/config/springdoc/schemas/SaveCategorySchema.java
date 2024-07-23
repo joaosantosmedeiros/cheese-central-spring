0f256f61b6ef0c39a07b933f1e4d9c5b65ea4e21
joaosantosmedeiros <jopesame@gmail.com>
2024-07-23 19:25:26 -0300

    docs: add create category controller springdoc definition

---
+++ src/main/java/joao/pedro/productsapi/infrastructure/config/springdoc/schemas/SaveCategorySchema.java
@@ -0,0 +1,14 @@
+package joao.pedro.productsapi.infrastructure.config.springdoc.schemas;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class SaveCategorySchema {
+    private String name;
+}
--- src/main/java/joao/pedro/productsapi/infrastructure/controllers/category/CreateCategoryController.java
+++ src/main/java/joao/pedro/productsapi/infrastructure/controllers/category/CreateCategoryController.java
@@ -1,8 +1,17 @@
 package joao.pedro.productsapi.infrastructure.controllers.category;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import joao.pedro.productsapi.entity.category.model.Category;
+import joao.pedro.productsapi.infrastructure.config.springdoc.schemas.DefaultErrorResponseSchema;
+import joao.pedro.productsapi.infrastructure.config.springdoc.schemas.SaveCategorySchema;
+import joao.pedro.productsapi.infrastructure.dtos.StandardResponse;
 import joao.pedro.productsapi.usecase.category.CreateCategoryUseCase;
 import joao.pedro.productsapi.usecase.category.CreateCategoryUseCase.Input;
 import lombok.AllArgsConstructor;
@@ -14,17 +23,33 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @AllArgsConstructor
+@Tag(name = "Categories", description = "Operations related to categories.")
+@SecurityRequirement(name = "bearer-key")
 public class CreateCategoryController {
 
     private final CreateCategoryUseCase createCategoryUseCase;
 
     @PostMapping("/category")
-    public ResponseEntity<Response> create(@RequestBody @Valid Request request){
+    @Operation(
+            description = "Category creation", summary = "Create a category",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = SaveCategorySchema.class))
+            ),
+            responses = {
+                    @ApiResponse(responseCode = "201", description = "Category created successfully."),
+                    @ApiResponse(responseCode = "400", description = "Invalid data provided.",
+                            content = @Content(schema = @Schema(implementation = StandardResponse.class))),
+                    @ApiResponse(responseCode = "403", description = "Forbidden access.", content = @Content()),
+                    @ApiResponse(responseCode = "409", description = "Category already exists.",
+                            content = @Content(schema = @Schema(implementation = DefaultErrorResponseSchema.class))),
+            }
+    )
+    public ResponseEntity<StandardResponse<Category>> create(@RequestBody @Valid Request request){
         var output = createCategoryUseCase.execute(new Input(request.name()));
 
-        return ResponseEntity.status(HttpStatus.CREATED).body(new Response(
-                true,
+        return ResponseEntity.status(HttpStatus.CREATED).body(new StandardResponse<>(
                 "Category created sucessfully.",
+                true,
                 output.data())
         );
     }
@@ -33,10 +58,4 @@ public class CreateCategoryController {
             @NotBlank
             String name
     ){}
-
-    public record Response(
-        Boolean status,
-        String message,
-        Category data
-    ) {}
 }
