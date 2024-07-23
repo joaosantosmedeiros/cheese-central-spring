c7ddb4499c1a40b61b745cca349d07472d3cf4c4
joaosantosmedeiros <jopesame@gmail.com>
2024-07-23 08:58:04 -0300

    docs: add list accounts controller spring doc definition

--- src/main/java/joao/pedro/productsapi/infrastructure/controllers/account/ListAccountsController.java
+++ src/main/java/joao/pedro/productsapi/infrastructure/controllers/account/ListAccountsController.java
@@ -1,6 +1,15 @@
 package joao.pedro.productsapi.infrastructure.controllers.account;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import joao.pedro.productsapi.entity.account.model.FetchedAccount;
+import joao.pedro.productsapi.infrastructure.config.springdoc.schemas.DefaultErrorResponseSchema;
+import joao.pedro.productsapi.infrastructure.dtos.StandardResponse;
 import joao.pedro.productsapi.usecase.account.ListAccountsUseCase;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +18,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@SecurityRequirement(name = "bearer-key")
+@Tag(name = "Accounts", description = "Operations related to accounts.")
 @RestController
 @AllArgsConstructor
 public class ListAccountsController {
@@ -16,17 +27,30 @@ public class ListAccountsController {
     private ListAccountsUseCase listAccountsUseCase;
 
     @GetMapping("/account")
-    public ResponseEntity<Response> listAccounts() {
+    @Operation(description = "Accounts listing.", summary = "List all accounts." )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Success."
+            ),
+            @ApiResponse(
+                    responseCode = "403",
+                    description = "Forbidden access",
+                    content = @Content()
+            ),
+    })
+    public ResponseEntity<StandardResponse<List<FetchedAccount>>> listAccounts() {
         var fetchedAccounts = listAccountsUseCase.execute()
                 .data()
                 .stream()
-                .map(account -> new FetchedAccount(account))
+                .map(FetchedAccount::new)
                 .toList();
 
-        return ResponseEntity.ok(new Response(fetchedAccounts));
-    }
-
-    public record Response(List<FetchedAccount> data) {
+        return ResponseEntity.ok(new StandardResponse<>(
+                "Listing accounts.",
+                true,
+                fetchedAccounts
+        ));
     }
 
 }
