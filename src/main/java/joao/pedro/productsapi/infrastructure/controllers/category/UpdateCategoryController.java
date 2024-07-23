05852f18b882d49327e5351532578277930ca7e9
joaosantosmedeiros <jopesame@gmail.com>
2024-07-23 19:37:56 -0300

    docs: add update category controller springdoc definition

--- src/main/java/joao/pedro/productsapi/infrastructure/controllers/category/UpdateCategoryController.java
+++ src/main/java/joao/pedro/productsapi/infrastructure/controllers/category/UpdateCategoryController.java
@@ -1,8 +1,17 @@
 package joao.pedro.productsapi.infrastructure.controllers.category;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import joao.pedro.productsapi.entity.category.model.Category;
+import joao.pedro.productsapi.infrastructure.config.springdoc.schemas.DefaultErrorResponseSchema;
+import joao.pedro.productsapi.infrastructure.config.springdoc.schemas.SaveCategorySchema;
+import joao.pedro.productsapi.infrastructure.dtos.StandardResponse;
 import joao.pedro.productsapi.usecase.category.UpdateCategoryUseCase;
 import joao.pedro.productsapi.usecase.category.UpdateCategoryUseCase.Input;
 import lombok.AllArgsConstructor;
@@ -17,29 +26,40 @@ import java.util.UUID;
 
 @RestController
 @AllArgsConstructor
+@Tag(name = "Categories", description = "Operations related to categories.")
+@SecurityRequirement(name = "bearer-key")
 public class UpdateCategoryController {
 
     private final UpdateCategoryUseCase updateCategoryUseCase;
 
     @PutMapping("/category/{id}")
-    public ResponseEntity<Response> updateCategory(@PathVariable("id") UUID id, @RequestBody @Valid Request request){
-        var output = this.updateCategoryUseCase.execute(new Input(id, request.name()));
+    @Operation(description = "Category update", summary = "Update a category",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    content = @Content(schema = @Schema(implementation = SaveCategorySchema.class))),
+            responses = {
+                @ApiResponse(responseCode = "200", description = "Success."),
+                @ApiResponse(responseCode = "400", description = "Invalid data provided.",
+                        content = @Content(schema = @Schema(implementation = DefaultErrorResponseSchema.class))),
+                @ApiResponse(responseCode = "403", description = "Forbidden access.", content = @Content()),
+                @ApiResponse(responseCode = "404", description = "Category not found.",
+                        content = @Content(schema = @Schema(implementation = DefaultErrorResponseSchema.class))),
+                @ApiResponse(responseCode = "409", description = "Category already exists.",
+                        content = @Content(schema = @Schema(implementation = DefaultErrorResponseSchema.class))),
+            }
+    )
+    public ResponseEntity<StandardResponse<Category>> updateCategory(@PathVariable("id") UUID id, @RequestBody @Valid Request request){
+        var output = this.updateCategoryUseCase.execute(new Input(new Category(id, request.name)));
 
-        return ResponseEntity.status(HttpStatus.OK).body(new Response(
-                true,
+        return ResponseEntity.status(HttpStatus.OK).body(new StandardResponse<>(
                 "Category updated sucessfully.",
+                true,
                 output.data()
         ));
     }
 
-    private record Request(
+    public record Request(
             @NotBlank
             String name
     ){}
 
-    private record Response(
-            Boolean status,
-            String message,
-            Category data
-    ){}
 }
