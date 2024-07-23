0006b1a9c511c007317aca3a19ddbabe0c5ccc3d
joaosantosmedeiros <jopesame@gmail.com>
2024-07-23 19:28:13 -0300

    docs: add list categories controller springdoc definition

--- src/main/java/joao/pedro/productsapi/infrastructure/controllers/category/ListCategoriesController.java
+++ src/main/java/joao/pedro/productsapi/infrastructure/controllers/category/ListCategoriesController.java
@@ -1,6 +1,10 @@
 package joao.pedro.productsapi.infrastructure.controllers.category;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import joao.pedro.productsapi.entity.category.model.Category;
+import joao.pedro.productsapi.infrastructure.dtos.StandardResponse;
 import joao.pedro.productsapi.usecase.category.ListCategoryUseCase;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -12,23 +16,21 @@ import java.util.List;
 
 @RestController
 @AllArgsConstructor
+@Tag(name = "Categories", description = "Operations related to categories.")
 public class ListCategoriesController {
 
     private final ListCategoryUseCase listCategoryUseCase;
 
     @GetMapping("/category")
-    public ResponseEntity<Response> listCategories() {
+    @Operation(description = "Categories listing", summary = "List all categories", responses = {
+            @ApiResponse(responseCode = "200", description = "Success")
+    })
+    public ResponseEntity<StandardResponse<List<Category>>> listCategories() {
         var output = listCategoryUseCase.execute();
-        return ResponseEntity.status(HttpStatus.OK).body(new Response(
-                true,
+        return ResponseEntity.status(HttpStatus.OK).body(new StandardResponse<>(
                 "Listing all categories.",
+                true,
                 output.data()
         ));
     }
-
-    private record Response(
-            Boolean status,
-            String message,
-            List<Category> data
-    ) {}
 }
