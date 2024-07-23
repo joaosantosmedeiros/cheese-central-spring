35e420cfd2c6dc02cdec9bd3e3c6c9859cdf75a0
joaosantosmedeiros <jopesame@gmail.com>
2024-07-23 14:15:05 -0300

    docs: add list product controller springdoc definition

--- src/main/java/joao/pedro/productsapi/infrastructure/controllers/product/ListProductsController.java
+++ src/main/java/joao/pedro/productsapi/infrastructure/controllers/product/ListProductsController.java
@@ -1,8 +1,12 @@
 package joao.pedro.productsapi.infrastructure.controllers.product;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import joao.pedro.productsapi.entity.product.model.Product;
+import joao.pedro.productsapi.infrastructure.dtos.StandardResponse;
 import joao.pedro.productsapi.usecase.product.ListProductUseCase;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,24 +14,20 @@ import java.util.List;
 
 @RestController
 @AllArgsConstructor
+@Tag(name = "Products", description = "Operation related to products.")
 public class ListProductsController {
 
     private final ListProductUseCase listProductUseCase;
 
     @GetMapping("/product")
-    public Response listProducts() {
+    @Operation(description = "Products listing", summary = "List all products")
+    public ResponseEntity<StandardResponse<List<Product>>> listProducts() {
         var output = listProductUseCase.execute();
 
-        return new Response(
-                true,
+        return ResponseEntity.ok(new StandardResponse<>(
                 "Listing all products.",
+                true,
                 output.data()
-        );
+        ));
     }
-
-    public record Response(
-            boolean status,
-            String message,
-            List<Product> data
-    ) {}
 }
