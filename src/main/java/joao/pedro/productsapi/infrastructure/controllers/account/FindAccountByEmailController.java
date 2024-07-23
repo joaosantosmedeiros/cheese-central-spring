48a8d675a440aa64725620926824d67bfcf79dc0
joaosantosmedeiros <jopesame@gmail.com>
2024-07-23 09:05:06 -0300

    docs: add find account by email springdoc definition

--- src/main/java/joao/pedro/productsapi/infrastructure/controllers/account/FindAccountByEmailController.java
+++ src/main/java/joao/pedro/productsapi/infrastructure/controllers/account/FindAccountByEmailController.java
@@ -1,6 +1,15 @@
 package joao.pedro.productsapi.infrastructure.controllers.account;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import joao.pedro.productsapi.entity.account.model.FetchedAccount;
+import joao.pedro.productsapi.infrastructure.config.springdoc.schemas.DefaultErrorResponseSchema;
+import joao.pedro.productsapi.infrastructure.dtos.StandardResponse;
 import joao.pedro.productsapi.usecase.account.FindAccountByEmailUseCase;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -8,6 +17,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+@SecurityRequirement(name = "bearer-key")
+@Tag(name = "Accounts", description = "Operations related to accounts.")
 @RestController
 @AllArgsConstructor
 public class FindAccountByEmailController {
@@ -15,19 +26,28 @@ public class FindAccountByEmailController {
     private final FindAccountByEmailUseCase findAccountByEmailUseCase;
 
     @GetMapping("/account/{email}")
-    public ResponseEntity<Response> findAccount(@PathVariable(value = "email") String email) {
+    @Operation(description = "Account detailing", summary = "Find an account" )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Success."
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Account not found",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = DefaultErrorResponseSchema.class)
+                    )
+            ),
+    })
+    public ResponseEntity<StandardResponse<FetchedAccount>> findAccount(@PathVariable(value = "email") String email) {
        var output = findAccountByEmailUseCase.execute(new FindAccountByEmailUseCase.Input(email));
 
-       return ResponseEntity.ok(new Response(
+       return ResponseEntity.ok(new StandardResponse<>(
+               "Account found successfully.",
                true,
-               "Account found successfully",
                new FetchedAccount(output.data())
        ));
     }
-
-    public record Response(
-            boolean status,
-            String message,
-            FetchedAccount data
-    ) {}
 }
