24f1a4646c02ea266353af5407b4a9fbe76d8b35
joaosantosmedeiros <jopesame@gmail.com>
2024-07-23 08:48:49 -0300

    refactor: rename springdocs package

--- src/main/java/joao/pedro/productsapi/infrastructure/config/springdoc/SpringDocConfig.java
+++ src/main/java/joao/pedro/productsapi/infrastructure/config/springdoc/SpringDocConfig.java
@@ -1,4 +1,4 @@
-package joao.pedro.productsapi.infrastructure.config.springdocs;
+package joao.pedro.productsapi.infrastructure.config.springdoc;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
