4e295c36496614c34d575e5d367b5008f03a0b6a
joaosantosmedeiros <jopesame@gmail.com>
2024-07-24 13:07:38 -0300

    docs: add list oders controller springdoc definition

--- src/main/java/joao/pedro/productsapi/infrastructure/controllers/order/ListOrdersController.java
+++ src/main/java/joao/pedro/productsapi/infrastructure/controllers/order/ListOrdersController.java
@@ -1,7 +1,13 @@
 package joao.pedro.productsapi.infrastructure.controllers.order;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import joao.pedro.productsapi.entity.order.model.FetchedOrder;
 import joao.pedro.productsapi.infrastructure.config.db.schema.AccountEntity;
+import joao.pedro.productsapi.infrastructure.dtos.StandardResponse;
 import joao.pedro.productsapi.usecase.order.ListOrdersByAccountUseCase;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -15,22 +21,27 @@ import java.util.List;
 @RestController
 @RequestMapping("/orders")
 @AllArgsConstructor
+@Tag(name = "Orders", description = "Operations related to orders.")
+@SecurityRequirement(name = "bearer-key")
 public class ListOrdersController {
 
     private final ListOrdersByAccountUseCase listOrdersByAccountUseCase;
 
     @GetMapping
-    public ResponseEntity<Response> listOrders() {
+    @Operation(description = "Orders listing", summary = "List all user's orders.", responses = {
+            @ApiResponse(responseCode = "200", description = "Success."),
+            @ApiResponse(responseCode = "403", description = "Forbidden access.", content = @Content()),
+    })
+    public ResponseEntity<StandardResponse<List<FetchedOrder>>> listOrders() {
         var account = ((AccountEntity) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).toAccount();
 
         List<FetchedOrder> orders = listOrdersByAccountUseCase.execute(new ListOrdersByAccountUseCase.Input(account)).data();
 
-        return ResponseEntity.ok(new Response(
+        return ResponseEntity.ok(new StandardResponse<>(
                 "Showing found orders.",
                 true,
                 orders
         ));
     }
 
-    public record Response(String message, boolean status, List<FetchedOrder> data) {}
 }
