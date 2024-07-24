1eb77b55e8f02832fc7838ae4789ae07849b2a99
joaosantosmedeiros <jopesame@gmail.com>
2024-07-24 09:52:19 -0300

    docs: add delete category controller springdoc definition

--- src/main/java/joao/pedro/productsapi/infrastructure/controllers/category/DeleteCategoryController.java
+++ src/main/java/joao/pedro/productsapi/infrastructure/controllers/category/DeleteCategoryController.java
@@ -1,5 +1,12 @@
 package joao.pedro.productsapi.infrastructure.controllers.category;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import joao.pedro.productsapi.infrastructure.config.springdoc.schemas.DefaultErrorResponseSchema;
 import joao.pedro.productsapi.usecase.category.DeleteCategoryUseCase;
 import joao.pedro.productsapi.usecase.category.DeleteCategoryUseCase.Input;
 import lombok.AllArgsConstructor;
@@ -13,12 +20,22 @@ import java.util.UUID;
 
 @RestController
 @AllArgsConstructor
+@Tag(name = "Categories", description = "Operations related to categories.")
+@SecurityRequirement(name = "bearer-key")
 public class DeleteCategoryController {
 
     private final DeleteCategoryUseCase deleteCategoryUseCase;
 
     @DeleteMapping("/category/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
+    @Operation(description = "Category delete", summary = "Delete a category", responses = {
+            @ApiResponse(responseCode = "204", description = "Succcess."),
+            @ApiResponse(responseCode = "403", description = "Forbidden access.", content = @Content),
+            @ApiResponse(responseCode = "404", description = "Category not found.",
+                    content = @Content(schema = @Schema(implementation = DefaultErrorResponseSchema.class))),
+            @ApiResponse(responseCode = "409", description = "Category is alrady in use.",
+                    content = @Content(schema = @Schema(implementation = DefaultErrorResponseSchema.class))),
+    })
     public void deleteCategory(@PathVariable("id") UUID id){
         this.deleteCategoryUseCase.execute(new Input(id));
     }
