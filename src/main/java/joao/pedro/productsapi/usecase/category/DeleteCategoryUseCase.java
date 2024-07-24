302d84a13d7f37f3c2c8e5fd77d897c61714d6b0
joaosantosmedeiros <jopesame@gmail.com>
2024-07-24 09:52:15 -0300

    feat: add product verification in delete category use case

--- src/main/java/joao/pedro/productsapi/usecase/category/DeleteCategoryUseCase.java
+++ src/main/java/joao/pedro/productsapi/usecase/category/DeleteCategoryUseCase.java
@@ -2,29 +2,32 @@ package joao.pedro.productsapi.usecase.category;
 
 import joao.pedro.productsapi.entity.category.gateway.CategoryGateway;
 import joao.pedro.productsapi.entity.exceptions.EntityNotFoundException;
+import joao.pedro.productsapi.entity.exceptions.ObjectInUseException;
+import joao.pedro.productsapi.entity.product.gateway.ProductGateway;
 
 import java.util.UUID;
 
 public class DeleteCategoryUseCase {
 
     private final CategoryGateway categoryGateway;
+    private final ProductGateway productGateway;
 
-    public DeleteCategoryUseCase(CategoryGateway categoryGateway){
+    public DeleteCategoryUseCase(CategoryGateway categoryGateway, ProductGateway productGateway) {
         this.categoryGateway = categoryGateway;
+        this.productGateway = productGateway;
     }
 
     public void execute(Input input) {
 
-        var categoryExists = this.categoryGateway.findById(input.id());
-        if(categoryExists.isEmpty()){
-            throw new EntityNotFoundException("Category");
+        var categoryExists = this.categoryGateway.findById(input.id()).orElseThrow(() -> new EntityNotFoundException("Category"));
+
+        if(!productGateway.findByCategory(categoryExists.toCategory()).isEmpty()){
+            throw new ObjectInUseException("Category") ;
         }
 
-        this.categoryGateway.delete(categoryExists.get());
+        this.categoryGateway.delete(categoryExists);
     }
 
-    public record Input(
-            UUID id
-    ){}
+    public record Input(UUID id){}
 
 }
--- src/test/java/joao/pedro/productsapi/usecase/category/DeleteCategoryUseCaseTest.java
+++ src/test/java/joao/pedro/productsapi/usecase/category/DeleteCategoryUseCaseTest.java
@@ -4,6 +4,9 @@ import joao.pedro.productsapi.entity.category.gateway.CategoryGateway;
 import joao.pedro.productsapi.entity.category.model.Category;
 import joao.pedro.productsapi.entity.exceptions.EntityAlreadyExistsException;
 import joao.pedro.productsapi.entity.exceptions.EntityNotFoundException;
+import joao.pedro.productsapi.entity.exceptions.ObjectInUseException;
+import joao.pedro.productsapi.entity.product.gateway.ProductGateway;
+import joao.pedro.productsapi.entity.product.model.Product;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -15,6 +18,7 @@ import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -22,6 +26,9 @@ class DeleteCategoryUseCaseTest {
 
     @Mock
     private CategoryGateway categoryGateway;
+
+    @Mock
+    private ProductGateway productGateway;
 
     @Autowired
     @InjectMocks
@@ -35,16 +42,17 @@ class DeleteCategoryUseCaseTest {
     @Test
     @DisplayName("Should delete a category successfully")
     void deleteCategorySuccess() {
-        UUID id = UUID.randomUUID();
+        Category category = new Category("any_name");
 
-        when(categoryGateway.findById(id)).thenReturn(Optional.of(new Category("random category")));
-
+        when(categoryGateway.findById(category.getId())).thenReturn(Optional.of(category));
+        when(productGateway.findByCategory(category)).thenReturn(List.of());
         assertDoesNotThrow(() -> {
-            deleteCategoryUseCase.execute(new DeleteCategoryUseCase.Input(id));
+            deleteCategoryUseCase.execute(new DeleteCategoryUseCase.Input(category.getId()));
         });
 
         verify(categoryGateway, times(1)).delete(any());
     }
+
     @Test
     @DisplayName("Should throw if no category is found")
     void deleteCategoryError() {
@@ -57,5 +65,19 @@ class DeleteCategoryUseCaseTest {
         });
 
         assertEquals("Category not found.", thrown.getMessage());
+    }
+
+    @Test
+    @DisplayName("Should throw if the category is in sue")
+    void deleteCategoryInUse() {
+        Category category = new Category("any_name");
+
+        when(categoryGateway.findById(category.getId())).thenReturn(Optional.of(category));
+        when(productGateway.findByCategory(category)).thenReturn(List.of(new Product("name", "desc", "image", 2d, category)));
+        Exception thrown = assertThrows(ObjectInUseException.class ,() -> {
+            deleteCategoryUseCase.execute(new DeleteCategoryUseCase.Input(category.getId()));
+        });
+
+        assertEquals("Category already in use.", thrown.getMessage());
     }
 }
