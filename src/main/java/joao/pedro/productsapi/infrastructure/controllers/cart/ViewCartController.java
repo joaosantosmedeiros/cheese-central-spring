14e91f69c781ffea9254be94b16f91ea4bbe232c
joaosantosmedeiros <jopesame@gmail.com>
2024-07-24 10:06:13 -0300

    docs: add view cart controller springdoc definition

--- src/main/java/joao/pedro/productsapi/infrastructure/controllers/cart/ViewCartController.java
+++ src/main/java/joao/pedro/productsapi/infrastructure/controllers/cart/ViewCartController.java
@@ -1,9 +1,17 @@
 package joao.pedro.productsapi.infrastructure.controllers.cart;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import joao.pedro.productsapi.entity.account.model.Account;
 import joao.pedro.productsapi.entity.cart.model.Cart;
 import joao.pedro.productsapi.entity.cartProduct.model.FetchedCartProduct;
 import joao.pedro.productsapi.infrastructure.config.db.schema.AccountEntity;
+import joao.pedro.productsapi.infrastructure.config.springdoc.schemas.DefaultErrorResponseSchema;
+import joao.pedro.productsapi.infrastructure.dtos.StandardResponse;
 import joao.pedro.productsapi.usecase.account.FindAccountByEmailUseCase;
 import joao.pedro.productsapi.usecase.cart.FindActiveCartByAccountIdUseCase;
 import joao.pedro.productsapi.usecase.cartProduct.FindCartProductByCartUseCase;
@@ -13,32 +21,33 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @AllArgsConstructor
+@Tag(name = "Carts", description = "Operation related to carts.")
+@SecurityRequirement(name = "bearer-key")
 public class ViewCartController {
 
-    private final FindAccountByEmailUseCase findAccountByEmailUseCase;
     private final FindActiveCartByAccountIdUseCase findActiveCartByAccountIdUseCase;
     private final FindCartProductByCartUseCase findCartProductByCartUseCase;
 
     @GetMapping("/cart/view")
-    public ResponseEntity<Response> viewItems() {
-        var tokenEmail = ((AccountEntity) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getEmail();
-        Account account = findAccountByEmailUseCase.execute(new FindAccountByEmailUseCase.Input(tokenEmail)).data();
+    @Operation(description = "Cart detail", summary = "Detail a cart", responses = {
+            @ApiResponse(responseCode = "200", description = "Success"),
+            @ApiResponse(responseCode = "403", description = "Forbidden access.", content = @Content()),
+            @ApiResponse(responseCode = "404", description = "Cart not found.",
+                    content = @Content(schema = @Schema(implementation = DefaultErrorResponseSchema.class))),
+    })
+    public ResponseEntity<StandardResponse<List<FetchedCartProduct>>> viewItems() {
+        var account = ((AccountEntity) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).toAccount();
         Cart cart = findActiveCartByAccountIdUseCase.execute(new FindActiveCartByAccountIdUseCase.Input(account.getId())).data();
-
         var cartProducts = findCartProductByCartUseCase.execute(new FindCartProductByCartUseCase.Input(cart.getId()));
 
-        return ResponseEntity.ok(new Response(
+        return ResponseEntity.ok(new StandardResponse<>(
                 "Showing found cart items.",
                 true,
                 cartProducts.data()
         ));
     }
-
-    public record Response(
-            String message,
-            boolean status,
-            FetchedCartProduct[] data
-    ) {}
 }
--- src/main/java/joao/pedro/productsapi/usecase/cartProduct/FindCartProductByCartUseCase.java
+++ src/main/java/joao/pedro/productsapi/usecase/cartProduct/FindCartProductByCartUseCase.java
@@ -3,6 +3,7 @@ package joao.pedro.productsapi.usecase.cartProduct;
 import joao.pedro.productsapi.entity.cartProduct.gateway.CartProductGateway;
 import joao.pedro.productsapi.entity.cartProduct.model.FetchedCartProduct;
 
+import java.util.List;
 import java.util.UUID;
 
 public class FindCartProductByCartUseCase {
@@ -14,12 +15,10 @@ public class FindCartProductByCartUseCase {
     }
 
     public Output execute(Input input) {
-        var cartProducts = cartProductGateway.findByCartId(input.cartId).stream().map(FetchedCartProduct::new)
-                .toList().toArray(new FetchedCartProduct[0]);
-
+        var cartProducts = cartProductGateway.findByCartId(input.cartId).stream().map(FetchedCartProduct::new).toList();
         return new Output(cartProducts);
     }
 
     public record Input(UUID cartId) {}
-    public record Output(FetchedCartProduct[] data) {}
+    public record Output(List<FetchedCartProduct> data) {}
 }
